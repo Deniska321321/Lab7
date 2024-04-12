3445a0a1b7fb3a5d9ee6ddd6a50e26b5f2abdf53
Your Name <you@example.com>
2024-04-12 16:59:22 +0300

    bassssssss

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.topic2.android.notes
 
 import android.annotation.SuppressLint
@@ -5,22 +6,16 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.rememberCoroutineScope
-import com.topic2.android.notes.routing.Screen
+import com.topic2.android.notes.screens.NotesScreen
 import com.topic2.android.notes.theme.NotesTheme
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
-import kotlinx.coroutines.launch
-import ui.components.AppDrawer
-import ui.components.Note
+
 
 /**
  * Main activity приложения.
  */
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
 
   private val viewModel: MainViewModel by viewModels(factoryProducer = {
     MainViewModelFactory(
@@ -34,21 +29,7 @@ class MainActivity : AppCompatActivity()  {
 
     setContent {
       NotesTheme {
-        val coroutineScope = rememberCoroutineScope()
-        val scaffoldState: ScaffoldState = rememberScaffoldState()
-        Scaffold(
-          scaffoldState = scaffoldState,
-          drawerContent = {
-            AppDrawer(
-              currentScreen = Screen.Notes,
-              closeDrawerAction = {
-                coroutineScope.launch {
-                  scaffoldState.drawerState.close()
-                }
-              }
-            ) },
-          content = {Note()}
-        )
+        NotesScreen(viewModel = viewModel)
       }
     }
   }
